5e61eb30c077b9157e0137489ed6c638963a3a11
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-07-04 18:21:35 -0400

    Loop for scalability

--- Terraform/nicoxmcdportfolio/validation.tf
+++ Terraform/nicoxmcdportfolio/validation.tf
@@ -1,10 +1,19 @@
 resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options.resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options.resource_record_type
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
   zone_id = aws_route53_zone.main.zone_id
-  records = [aws_acm_certificate.cert.domain_validation_options.resource_record_value]
+  records = [each.value.record]
   ttl     = 300
 }
+
 
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
