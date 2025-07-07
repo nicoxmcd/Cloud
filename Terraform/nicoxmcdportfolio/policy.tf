9daa5a63808a57dd78cca0fc77ddcf24716ef8b6
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-07-07 10:21:16 -0400

    403

--- Terraform/nicoxmcdportfolio/policy.tf
+++ Terraform/nicoxmcdportfolio/policy.tf
@@ -24,10 +24,3 @@ resource "aws_s3_bucket_ownership_controls" "ownership" {
     object_ownership = "BucketOwnerEnforced"
   }
 }
-
-resource "aws_s3_bucket_acl" "acl" {
-  bucket = aws_s3_bucket.portfolio.id
-  acl    = "private"
-
-  depends_on = [aws_s3_bucket_ownership_controls.ownership]
-}
