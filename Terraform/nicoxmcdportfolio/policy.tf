92e0c810d7fba2630b24ac261112d0371767e300
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-07-07 10:58:13 -0400

    Update policy.tf

--- Terraform/nicoxmcdportfolio/policy.tf
+++ Terraform/nicoxmcdportfolio/policy.tf
@@ -24,3 +24,12 @@ resource "aws_s3_bucket_ownership_controls" "ownership" {
     object_ownership = "BucketOwnerEnforced"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.portfolio.id
+
+  block_public_acls       = true
+  block_public_policy     = false # Allow policy to work
+  ignore_public_acls      = true
+  restrict_public_buckets = false
+}
