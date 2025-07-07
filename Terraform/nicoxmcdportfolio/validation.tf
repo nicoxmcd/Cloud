e575b84a32c144a8009737ebc7d97d23e0b9cc5b
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-07-06 22:32:44 -0400

    Update validation.tf

--- Terraform/nicoxmcdportfolio/validation.tf
+++ Terraform/nicoxmcdportfolio/validation.tf
@@ -1,21 +1,12 @@
 resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      record = dvo.resource_record_value
-    }
-  }
-
-  name    = each.value.name
-  type    = each.value.type
+  name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
+  type    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
+  records = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
   zone_id = data.aws_route53_zone.main.zone_id
-  records = [each.value.record]
   ttl     = 300
 }
 
-
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
 }
