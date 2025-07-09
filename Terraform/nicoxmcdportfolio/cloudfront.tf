324f6d341ab8270097ccd954bf29d8f3a49c1fc9
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-07-08 23:30:42 -0400

    Custom endpoint no OAI

--- Terraform/nicoxmcdportfolio/cloudfront.tf
+++ Terraform/nicoxmcdportfolio/cloudfront.tf
@@ -1,34 +1,29 @@
-data "aws_cloudfront_origin_access_identity" "oai" {
-  id = "E283AV0I62ZFQR"
-}
-
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
-    domain_name = aws_s3_bucket.portfolio.website_endpoint
-    origin_id   = "S3-nicoxmcdportfolio"
+    domain_name = "${aws_s3_bucket.portfolio.bucket}.s3-website.${var.region}.amazonaws.com"
+    origin_id   = "S3-${aws_s3_bucket.portfolio.bucket}"
 
     custom_origin_config {
       http_port              = 80
       https_port             = 443
-      origin_protocol_policy = "http-only" # Required for static website endpoints
+      origin_protocol_policy = "http-only" # required for S3 static website hosting
       origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
 
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "CloudFront distribution for nicoxmcdportfolio"
+  comment             = "CloudFront distribution for ${aws_s3_bucket.portfolio.bucket}"
   default_root_object = "index.html"
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-nicoxmcdportfolio"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3-${aws_s3_bucket.portfolio.bucket}"
     viewer_protocol_policy = "redirect-to-https"
 
     forwarded_values {
       query_string = false
-
       cookies {
         forward = "none"
       }
--- Terraform/nicoxmcdportfolio/policy.tf
+++ Terraform/nicoxmcdportfolio/policy.tf
@@ -5,32 +5,29 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid    = "AllowCloudFrontServiceGetObject",
-        Effect = "Allow",
-        Principal = {
-          AWS = data.aws_cloudfront_origin_access_identity.oai.iam_arn
-        },
-        Action   = "s3:GetObject",
-        Resource = "${aws_s3_bucket.portfolio.arn}/*"
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.portfolio.arn}/*"
       }
     ]
   })
 }
 
-
 resource "aws_s3_bucket_ownership_controls" "ownership" {
   bucket = aws_s3_bucket.portfolio.id
 
   rule {
-    object_ownership = "BucketOwnerEnforced"
+    object_ownership = "ObjectWriter"
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.portfolio.id
 
-  block_public_acls       = true
-  block_public_policy     = true    
-  ignore_public_acls      = true
-  restrict_public_buckets = true    
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
