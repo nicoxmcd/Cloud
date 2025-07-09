be5bea9b547b41485c6b34079999d69488982165
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-07-08 22:54:08 -0400

    Allow overwrite

--- Terraform/nicoxmcdportfolio/validation.tf
+++ Terraform/nicoxmcdportfolio/validation.tf
@@ -1,4 +1,5 @@
 resource "aws_route53_record" "cert_validation" {
+  allow_overwrite = true
   name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
   type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
   records = [tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value]
