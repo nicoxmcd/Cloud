25726011b56e3e5cd1237623cf95248a651d6647
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-07-08 22:08:23 -0400

    block public access

--- Terraform/nicoxmcdportfolio/policy.tf
+++ Terraform/nicoxmcdportfolio/policy.tf
@@ -2,20 +2,21 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.portfolio.id
 
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Sid       = "AllowCloudFrontServiceGetObject",
-        Effect = "Allow"
+        Sid    = "AllowCloudFrontServiceGetObject",
+        Effect = "Allow",
         Principal = {
           AWS = aws_cloudfront_origin_access_identity.oai.iam_arn
-        }
-        Action = "s3:GetObject"
+        },
+        Action   = "s3:GetObject",
         Resource = "${aws_s3_bucket.portfolio.arn}/*"
       }
     ]
   })
 }
+
 
 resource "aws_s3_bucket_ownership_controls" "ownership" {
   bucket = aws_s3_bucket.portfolio.id
@@ -29,7 +30,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.portfolio.id
 
   block_public_acls       = true
-  block_public_policy     = false # Allow policy to work
+  block_public_policy     = true    
   ignore_public_acls      = true
-  restrict_public_buckets = false
+  restrict_public_buckets = true    
 }
