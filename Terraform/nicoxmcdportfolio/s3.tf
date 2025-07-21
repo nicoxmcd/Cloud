b4d51c0e9c78e3667a854d269578cace4550535c
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-07-21 18:17:51 -0400

    Update s3.tf

--- Terraform/nicoxmcdportfolio/s3.tf
+++ Terraform/nicoxmcdportfolio/s3.tf
@@ -9,10 +9,10 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
   bucket = aws_s3_bucket.portfolio.bucket
 
   index_document {
-    suffix = "index.astro"
+    suffix = "index.html"
   }
 
   error_document {
-    key = "404.astro"
+    key = "404.html"
   }
 }
