4ec93b7107114b8503605e81bb24d2f1622e26a3
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-08-08 17:44:46 -0400

    Re-added lifecycle policy

--- Terraform/nicoxmcdportfolio/s3.tf
+++ Terraform/nicoxmcdportfolio/s3.tf
@@ -71,3 +71,26 @@ resource "aws_s3_bucket_cors_configuration" "cors" {
     max_age_seconds = 3000
   }
 }
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.portfolio.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "versioning_lifecycle" {
+  bucket = aws_s3_bucket.portfolio.id
+  
+  rule {
+    id     = "expire-noncurrent-versions"
+    status = "Enabled"
+
+    filter {}
+
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+  }
+}
