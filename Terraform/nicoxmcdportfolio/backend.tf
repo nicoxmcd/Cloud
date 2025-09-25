b4fc84f221196ffe975faafabaf11726c600713c
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2025-09-24 23:01:17 -0400

    Update backend.tf

--- Terraform/nicoxmcdportfolio/backend.tf
+++ Terraform/nicoxmcdportfolio/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "nicoxmcdportfolio-tfstate"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    use_lockfile   = true
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
