5aa1276be8f1a50d2e9aa776a4743fa709c4ef6f
Nicole An <35404943+nicoxmcd@users.noreply.github.com>
2025-12-05 22:13:35 -0500

    Update certificate.tf

--- Terraform/nicolexanportfolio/certificate.tf
+++ Terraform/nicolexanportfolio/certificate.tf
@@ -17,7 +17,7 @@ resource "aws_route53_record" "cert_validation" {
   name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
   type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
   records = [tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value]
-  zone_id = data.aws_route53_zone.main.zone_id
+  zone_id = aws_route53_zone.main.zone_id
   ttl     = 300
 }
 
@@ -26,7 +26,7 @@ resource "aws_route53_record" "cert_validation_www" {
   name            = tolist(aws_acm_certificate.cert.domain_validation_options)[1].resource_record_name
   type            = tolist(aws_acm_certificate.cert.domain_validation_options)[1].resource_record_type
   records         = [tolist(aws_acm_certificate.cert.domain_validation_options)[1].resource_record_value]
-  zone_id         = data.aws_route53_zone.main.zone_id
+  zone_id         = aws_route53_zone.main.zone_id
   ttl             = 300
 }
 
