9f2f7f190871fc48ddaf853107c315103ef2dc64
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2026-02-22 20:26:18 -0500

    Update lambda.tf

--- Terraform/nicolexanportfolio/lambda.tf
+++ Terraform/nicolexanportfolio/lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda_exec_role" {
-  name = "portfolioLambdaRole"
+  name = "nicolexanportfolioLambdaRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
