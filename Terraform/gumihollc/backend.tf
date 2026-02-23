d97428e2a6991c5352ba6539c5eaa1cfa254321d
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2026-02-22 21:21:53 -0500

    remove gumiho iac

--- Terraform/gumihollc/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "state" {
-    bucket         = "gumihollc-tfstate"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
