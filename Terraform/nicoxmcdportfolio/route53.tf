8f4a9ef2aef940c27cb34cd9e92b01eb5c6745cb
Nicole X McDermott <35404943+nicoxmcd@users.noreply.github.com>
2026-02-22 23:09:16 -0500

    new hosted zone

--- Terraform/nicoxmcdportfolio/route53.tf
+++ Terraform/nicoxmcdportfolio/route53.tf
@@ -1,25 +1,25 @@
-data "aws_route53_zone" "main" {
-  name         = var.domain_name
-  private_zone = false
+resource "aws_route53_zone" "main" {
+  name = var.domain_name
+
+  tags = {
+    Project = var.domain_name
+  }
 }
 
-# Alias using DNS records pointing to CloudFront not directly to S3
-# Direct www.xyz.com to www.xyz.com
 resource "aws_route53_record" "www_a" {
-  zone_id = data.aws_route53_zone.main.zone_id
+  zone_id = aws_route53_zone.main.zone_id
   name    = "www.${var.domain_name}"
   type    = "A"
 
   alias {
-    name    = aws_cloudfront_distribution.cdn.domain_name
-    zone_id = aws_cloudfront_distribution.cdn.hosted_zone_id
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
     evaluate_target_health = false
   }
 }
 
-# Redirect apex (xyz.com) to www.xyz.com
 resource "aws_route53_record" "apex_redirect" {
-  zone_id = data.aws_route53_zone.main.zone_id
+  zone_id = aws_route53_zone.main.zone_id
   name    = var.domain_name
   type    = "A"
 
